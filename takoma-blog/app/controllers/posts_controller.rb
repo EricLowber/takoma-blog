75e6c30e45e60b4c65e9bbc667fa1219870c3c8c
Eric Lowber <elowber@gmail.com>
2017-05-21 13:51:15 -0500

    added carrierwave

--- takoma-blog/app/controllers/posts_controller.rb
+++ takoma-blog/app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:edit, :update, :show, :delete]
   before_action :authenticate_admin!, except: [:index, :show]
+  before_action :find_post, only: [:edit, :update, :show, :delete]
+  
   
   # Index action to render all posts 
   def index
